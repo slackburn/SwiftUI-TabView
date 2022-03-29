845abdea081658bc14c72dda387b44be36ddc6d5
Samuel Blackburn <samuel.blackburn@nbrown.co.uk>
2022-03-29 13:17:33 +0100

    Initial Commit

---
+++ SwiftUI TabView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI TabView
+//
+//  Created by Samuel Blackburn on 29/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI TabView/SwiftUI_TabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TabViewApp.swift
+//  SwiftUI TabView
+//
+//  Created by Samuel Blackburn on 29/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
